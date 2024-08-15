0d3875ba45c7a55ef891c9bfe3c9fcb952d80ea4
Mark Hatle <mark.hatle@amd.com>
2024-08-14 22:16:50 -0600

    conf/machine: Update 2023.1 machine versions
    Create a new index file using:
      ./hdf-repository-generate-srcuri.sh https://petalinux.xilinx.com/sswreleases/rel-v2023/hdf-examples/2023.1/ | sort > 2023.1-xsa-index
    
    Generate updated machine files:
      (in the build-dir)
       ../sources/meta-xilinx-tools/scripts/generate-machines-2023.1.sh ../sources/meta-xilinx-tools/conf ../sources/meta-xilinx-tools/scripts/2023.1-xsa-index
    
    Signed-off-by: Mark Hatle <mark.hatle@amd.com>

--- scripts/xsa-index-generate-srcuri.sh
+++ scripts/xsa-index-generate-srcuri.sh
@@ -62,49 +62,9 @@ else
     cd ${urlpath}
 fi
 
-echo "# Automatically generated.  Manual changes will be lost."
-echo
-echo "# Redefine the default to use our values if not overriden by the user"
-echo "# fall back to the original default if necessary"
-echo "HDF_BASE_DEFAULT := '\${HDF_BASE}'"
-echo "HDF_PATH_DEFAULT := '\${HDF_PATH}'"
-echo "BRANCHARG_DEFAULT := '\${BRANCHARG}'"
-echo "HDF_BASE ??= \"\${@d.getVarFlag('HDF_BASE', d.getVar('HDF_MACHINE') or 'undefined') or '\${HDF_BASE_DEFAULT}'}\""
-echo "HDF_PATH ??= \"\${@d.getVarFlag('HDF_PATH', d.getVar('HDF_MACHINE') or 'undefined') or '\${HDF_PATH_DEFAULT}'}\""
-echo "BRANCHARG ??= \"\${@d.getVarFlag('BRANCHARG', d.getVar('HDF_MACHINE') or 'undefined') or '\${BRANCHARG_DEFAULT}'}\""
-
-# Downloaded but not used
-README="README.md"
-if [ ! -e ${README} ]; then
-    echo "WARNING: No ${README} file!" >&2
-else
-    echo "SRC_URI += '${url}/$README;name=readme'"
-    echo "SRC_URI[readme.sha256sum] = '$(sha256sum $README | cut -d ' ' -f 1)'"
-    echo
-fi
-
-# Include the files, but don't setup LIC_FILES_CHKSUM.  User can manually
-# file the LICENSE file if needed.
-LICENSE="LICENSE.md"
-if [ ! -e ${LICENSE} ]; then
-    echo "WARNING: No ${LICENSE} file!" >&2
-else
-    echo "SRC_URI += '${url}/$LICENSE;name=license'"
-    echo "SRC_URI[license.sha256sum] = '$(sha256sum $LICENSE | cut -d ' ' -f 1)'"
-
-    # Disable since it breaks current PetaLinux workflow
-    #if [ ${urlproto} = "file://" ]; then
-    #    echo "LIC_FILES_CHKSUM = 'file://${urlpath}/$LICENSE;md5=$(md5sum $LICENSE | cut -d ' ' -f 1)'"
-    #else
-    #    echo "LIC_FILES_CHKSUM = 'file://$LICENSE;md5=$(md5sum $LICENSE | cut -d ' ' -f 1)'"
-    echo
-fi
-
 for each_file in $(find . -type f -name '*.xsa' | sort) ; do
     case ${each_file} in
         *index.html)  continue ;;
-        *${README})   continue ;;
-        *${LICENSE})  continue ;;
         *)
             id=$(basename `dirname $each_file` | tr '/' '_')
             # Find subdirectories, if present
@@ -112,12 +72,7 @@ for each_file in $(find . -type f -name '*.xsa' | sort) ; do
             subdir=${subdir##.}
             file=$(basename $each_file)
             sha=$(sha256sum $each_file | cut -d ' ' -f 1)
-            echo
-            echo "# ${id}"
-            echo "HDF_BASE[${id}] = '${urlproto}'"
-            echo "HDF_PATH[${id}] = '${urlpath}${subdir}/${id}/${file}'"
-            echo "BRANCHARG[${id}] = 'name=${id}'"
-            echo "SRC_URI[${id}.sha256sum] = '${sha}'"
+            echo "${id} ${urlproto}${urlpath}${subdir}/${id}/${file}"
             ;;
     esac
 done
