c2d20ecfdc473cd271ee72e3737cb2e35c018874
Mark Hatle <mark.hatle@amd.com>
2024-10-31 15:02:23 -0600

    machines: Regenerate for 2025.1 development
    Signed-off-by: Mark Hatle <mark.hatle@amd.com>

--- scripts/generate-machines-qemu.sh
+++ scripts/generate-machines-qemu.sh
@@ -1,6 +1,6 @@
 #! /bin/bash -e
 
-XSCT_VERSION=2024.2
+XSCT_VERSION=2025.1
 
 ### The following table controls the automatic generated of the machine .conf files (lines start with #M#)
 ### Machine               Board Template                     PRE                                                                 POST
