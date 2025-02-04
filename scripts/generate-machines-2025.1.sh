8a9db3f5a555ab468aabbf7efee333d7815135c6
Mark Hatle <mark.hatle@amd.com>
2025-02-03 18:38:58 -0700

    2025.1 machines: Rename QB_XILINX_SERIAL to QEMU_HW_SERIAL and regenerate
    Signed-off-by: Mark Hatle <mark.hatle@amd.com>

--- scripts/generate-machines-2025.1.sh
+++ scripts/generate-machines-2025.1.sh
@@ -20,8 +20,8 @@ XSCT_VERSION=2025.1
 #M# vmk180-versal         versal-vmk180-reva-x-ebm-01-reva   QB_MEM = \"-m 8G\"\\nQEMU_HW_DTB_PS = \"\${QEMU_HW_DTB_PATH}/board-versal-ps-vmk180.dtb\"\\nQEMU_HW_DTB_PMC = \"${QEMU_HW_DTB_PATH}/board-versal-pmc-virt.dtb\"\\n
 #M# vpk120-versal         versal-vpk120-reva                 QB_MEM = \"-m 12G\"\\nQEMU_HW_DTB_PS = \"${QEMU_HW_DTB_PATH}/board-versal-ps-vpk120.dtb\"\\nQEMU_HW_DTB_PMC = \"${QEMU_HW_DTB_PATH}/board-versal-pmc-virt.dtb\"\\n
 #M# vpk180-versal         versal-vpk180-reva                 QB_MEM = \"-m 12G\"\\nQEMU_HW_DTB_PS = \"${QEMU_HW_DTB_PATH}/board-versal-ps-vpk180.dtb\"\\nQEMU_HW_DTB_PMC = \"${QEMU_HW_DTB_PATH}/board-versal-pmc-virt.dtb\"\\n
-#M# zc702-zynq7           zc702                              QB_MEM = \"-m 1024\"\\nQB_XILINX_SERIAL = \"-serial null -serial mon:stdio\"\\n
-#M# zc706-zynq7           zc706                              QB_MEM = \"-m 1024\"\\nQB_XILINX_SERIAL = \"-serial null -serial mon:stdio\"\\n
+#M# zc702-zynq7           zc702                              QB_MEM = \"-m 1024\"\\nQEMU_HW_SERIAL = \"-serial null -serial mon:stdio\"\\n
+#M# zc706-zynq7           zc706                              QB_MEM = \"-m 1024\"\\nQEMU_HW_SERIAL = \"-serial null -serial mon:stdio\"\\n
 #M# zcu102-zynqmp         zcu102-rev1.0                      QB_MEM = \"-m 4G\"\\nQEMU_HW_DTB_PS = \"\${QEMU_HW_DTB_PATH}/zcu102-arm.dtb\"\\nQEMU_HW_DTB_PMU = \"${QEMU_HW_DTB_PATH}/zynqmp-pmu.dtb\"\\n
 #M# zcu104-zynqmp         zcu104-revc                        QB_MEM = \"-m 4G\"\\nQEMU_HW_DTB_PS = \"\${QEMU_HW_DTB_PATH}/board-zynqmp-zcu104.dtb\"\\nQEMU_HW_DTB_PMU = \"${QEMU_HW_DTB_PATH}/zynqmp-pmu.dtb\"\\n
 #M# zcu106-zynqmp         zcu106-reva                        
