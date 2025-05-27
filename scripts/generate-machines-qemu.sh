746bb7b346eec325c2b33164ccb3399fa45bac8c
Mark Hatle <mark.hatle@amd.com>
2025-05-27 09:27:39 -0500

    scripts: Add missing 2025.2 references
    Signed-off-by: Mark Hatle <mark.hatle@amd.com>

--- scripts/generate-machines-qemu.sh
+++ scripts/generate-machines-qemu.sh
@@ -1,6 +1,6 @@
 #! /bin/bash -e
 
-XSCT_VERSION=2025.1
+XSCT_VERSION=2025.2
 
 ### The following table controls the automatic generated of the machine .conf files (lines start with #M#)
 ### Machine               Board Template                     PRE                                                                 POST
