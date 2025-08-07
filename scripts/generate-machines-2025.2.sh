0e0e3b6feff35a6e2a1a19d934c162face1140f0
Mark Hatle <mark.hatle@amd.com>
2025-08-07 10:20:53 -0500

    Remove 2025.2 zcu111, use SDT version
    Signed-off-by: Mark Hatle <mark.hatle@amd.com>

--- scripts/generate-machines-2025.2.sh
+++ scripts/generate-machines-2025.2.sh
@@ -15,7 +15,6 @@ XSCT_VERSION=2025.2
 #M# vpk180-versal         versal-vpk180-reva                 QB_MEM = \"-m 12G\"\\nQEMU_HW_DTB_PS = \"${QEMU_HW_DTB_PATH}/board-versal-ps-vpk180.dtb\"\\nQEMU_HW_DTB_PMC = \"${QEMU_HW_DTB_PATH}/board-versal-pmc-virt.dtb\"\\n
 #M# zc706-zynq7           zc706                              QB_MEM = \"-m 1024\"\\nQEMU_HW_SERIAL = \"-serial null -serial mon:stdio\"\\n
 #M# zcu102-zynqmp         zcu102-rev1.0                      QB_MEM = \"-m 4G\"\\nQEMU_HW_DTB_PS = \"\${QEMU_HW_DTB_PATH}/zcu102-arm.dtb\"\\nQEMU_HW_DTB_PMU = \"${QEMU_HW_DTB_PATH}/zynqmp-pmu.dtb\"\\n
-#M# zcu111-zynqmp         zcu111-reva                        
 #M# zcu208-sdfec-zynqmp   zcu208-reva                        
 #M# zcu208-zynqmp         zcu208-reva                        
 #M# zcu216-zynqmp         zcu216-reva                        
